9698477632b924e558889616275dbf278b1c76ff
Aurelien Lourot <software@lourot.dev>
2024-01-11 18:43:03 +0100

    fix: reorder things
    in order to be a reproducer showcasing progressive
    hydration not happening.

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,18 @@
 import { ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 const count = ref(0)
+
+// We expect Vue to server-side render the entire template first with the
+// Suspense's fallback and an interactive button first, and then later to stream
+// <script>s that will replace the fallback with the Suspense's default.
+// However it seems that instead Vue's renderer hangs on rendering the Suspense,
+// finally rendering the entire template with the Suspense's default. The
+// fallback is never rendered, and we're not getting an interactive button while
+// the renderer is hanging.
+// See https://github.com/vikejs/vike/discussions/1318
 </script>
 
 <template>
-  <button type="button" @click="count++">count is {{ count }}</button>
-
   <Suspense>
     <template #default>
       <HelloWorld />
@@ -15,4 +22,6 @@ const count = ref(0)
       <div>Loading ...</div>
     </template>
   </Suspense>
+
+  <button type="button" @click="count++">count is {{ count }}</button>
 </template>
